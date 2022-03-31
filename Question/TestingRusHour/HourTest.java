59aea81bfe12d8b4217055a63968ec7870e3a3c2
mohammed-730-saeed <mohd.maghribi202@gmail.com>
2022-03-31 19:37:29 +0300

    first commit

---
+++ Question/TestingRusHour/HourTest.java
@@ -0,0 +1,50 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HourTest {
+    Hour c;
+    @Before
+    public void setup() throws Exception{
+        c= new Hour();
+        System.out.print("---------------------");
+    }
+
+    @Test
+    public void Boundrytest1(){
+        String result =c.TrainTicketFare((float) 1.50);
+        assertEquals("Full Fare",result);
+    }
+    @Test
+    public void Boundrytest2(){
+        String result =c.TrainTicketFare((float ) 11);
+        assertEquals("Saver Ticket ",result);
+    }
+    @Test
+    public void Boundrytest3(){
+        String result =c.TrainTicketFare((float ) 11.30);
+        assertEquals("Saver Ticket ",result);
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void Boundrytest4(){
+        String result =c.TrainTicketFare((float ) -2);
+        assertEquals("Saver Ticket ",result);
+    }
+			//    public void Boundrytest2(){
+			//        String result =c.TrainTicketFare((float ) 11);
+			//        assertEquals("Saver Ticket ",result);
+			//    }
+			//    public void Boundrytest2(){
+			//        String result =c.TrainTicketFare((float ) 11);
+			//        assertEquals("Saver Ticket ",result);
+			//    }
+			//    public void Boundrytest2(){
+			//        String result =c.TrainTicketFare((float ) 11);
+			//        assertEquals("Saver Ticket ",result);
+			//    }
+			//    public void Boundrytest2(){
+			//        String result =c.TrainTicketFare((float ) 11);
+			//        assertEquals("Saver Ticket ",result);
+			//    }
+}
---
+++ Question/src/Hour.java
@@ -0,0 +1,12 @@
+public class Hour {
+    public String TrainTicketFare(float time){
+        String ticketPrice="null";
+        if(time<=0 || time>24)
+            throw  new IllegalArgumentException(" time enterd!!!");
+        if ((time>=1 && time<=9.50) || (time>=16 && time<=19.50) )
+            ticketPrice="Full Fare";
+        if ((time>9.50 && time<16) || (time>19.50 && time<=24) )
+            ticketPrice="Saver Ticket ";
+        return ticketPrice;
+    }
+}
